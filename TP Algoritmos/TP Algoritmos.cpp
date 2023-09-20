2876a17a778e88461c4f59dc7b114b33148ceca4
jahazielgpz <joseguepe10@gmail.com>
2023-09-19 23:12:03 -0500

    añadi el cod q avancé ayer

--- TP Algoritmos/TP Algoritmos.cpp
+++ TP Algoritmos/TP Algoritmos.cpp
@@ -1,9 +1,36 @@
 #include "pch.h"
+#include "askAndAnswers.h"
+#include "conio.h"
 
 using namespace System;
 
 int main()
 {
+    srand(time(0));
+    Pregunta* objPregunta;
+    ArrPreguntaRespondidas* arrPreguntas = new ArrPreguntaRespondidas;
+    int c = 1;
+    while (c == 1)
+    {
+        if (kbhit()) {
+            char tecla = _getch();
+            switch (tolower(tecla))
+            {
+            case 'a':
+                objPregunta = new Pregunta;
+                arrPreguntas->agregarPregunta(objPregunta);
+                break;
+            case 'b':
+                arrPreguntas->listarPreguntas();
+                break;
+            case 'c':
+                c = 2;
+                break;
+            default:
+                break;
+            }
+        }
+    }delete arrPreguntas;
 
     return 0;
 }

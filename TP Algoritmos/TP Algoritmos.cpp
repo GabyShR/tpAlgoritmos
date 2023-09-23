656581abb46adca90e5ae45060e6c63da4b37d92
GabyShR <gabrielashapiama285@gmail.com>
2023-09-23 14:19:33 -0500

    agregue animacion creditos

--- TP Algoritmos/TP Algoritmos.cpp
+++ TP Algoritmos/TP Algoritmos.cpp
@@ -1,10 +1,20 @@
 #include "pch.h"
-#include <iostream>
-using namespace System;
-using namespace std; 
+#include "CCreditos.h"
 
 int main()
 {
-    cout << "pov";
-    return 0;
+	//system("color 3b");
+	Console::SetWindowSize(WIDTH, HEIGHT);
+	Console::CursorVisible = false;
+	CCreditos* creditos = new CCreditos();
+	CIntegrante* gaby = new CIntegrante(5, 103);
+
+	//creditos->pantallaDeFondo();
+
+
+	creditos->animarNombres();
+
+
+	system("pause>0");
+	return 0;
 }
